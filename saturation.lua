658a3ed2ff8d8bc4f9d99efa2b1fd9a7c6d86097
Zoey <75682071+Skepyz@users.noreply.github.com>
2021-08-16 18:38:47 -0700

    Added the .lua file

---
+++ saturation.lua
@@ -0,0 +1,51 @@
+local cc = Instance.new("ColorCorrectionEffect")
+cc.Parent = game.Lighting
+local change = game.Lighting.ColorCorrection
+
+wait(1)
+
+change.Saturation = 1
+
+wait(1)
+
+change.Saturation = 0.9
+
+wait(1)
+
+change.Saturation = 0.8
+
+wait(1)
+
+change.Saturation = 0.7
+
+wait(1)
+
+change.Saturation = 0.6
+
+wait(1)
+
+change.Saturation = 0.5
+
+wait(1)
+
+change.Saturation = 0.4
+
+wait(1)
+
+change.Saturation = 0.3
+
+wait(1)
+
+change.Saturation = 0.2
+
+wait(1)
+
+change.Saturation = 0.1
+
+wait(1)
+
+change.Saturation = 0
+
+wait(1)
+
+change.Saturation = -1
