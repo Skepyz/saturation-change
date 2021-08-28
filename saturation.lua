bfa00b84e66c71c5c76c907d5454bafedcb1f056
Zoey <75682071+Skepyz@users.noreply.github.com>
2021-08-28 13:15:04 -0400

    update

--- saturation.lua
+++ saturation.lua
@@ -1,169 +1,58 @@
 local cc = Instance.new("ColorCorrectionEffect")
 cc.Parent = game.Lighting
 local change = game.Lighting.ColorCorrection
+repeat wait() until game.Players.LocalPlayer
 
-wait(1)
+m = game.Players.LocalPlayer:GetMouse()
 
-change.Saturation = 1
-
-wait(1)
-
-change.Saturation = 0.9
-
-wait(1)
-
-change.Saturation = 0.8
-
-wait(1)
-
-change.Saturation = 0.7
-
-wait(1)
-
-change.Saturation = 0.6
-
-wait(1)
-
-change.Saturation = 0.5
-
-wait(1)
-
-change.Saturation = 0.4
-
-wait(1)
-
-change.Saturation = 0.3
-
-wait(1)
-
-change.Saturation = 0.2
-
-wait(1)
-
-change.Saturation = 0.1
-
-wait(1)
-
-change.Saturation = 0
-
-wait(1)
-
+m.KeyDown:connect(function(key)
+    if key == "h" then --"Shift to run" 0 == shift
+        change.Saturation = 0
+wait(0.01)
 change.Saturation = -0.1
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.2
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.3
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.4
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.5
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.6
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.7
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.8
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.9
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -1
+    end
+end)
 
-wait(8)
-
-change.Saturation = -1
-
-wait(1)
-
+m.KeyUp:connect(function(key)
+    if key == "h" then
+        change.Saturation = -1
+wait(0.01)
 change.Saturation = -0.9
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.8
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.7
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.6
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.5
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.4
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.3
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.2
-
-wait(1)
-
+wait(0.01)
 change.Saturation = -0.1
-
-wait(1)
-
-change.Saturation = 0
-
-wait(1)
-
-change.Saturation = 0.1
-
-wait(1)
-
-change.Saturation = 0.2
-
-wait(1)
-
-change.Saturation = 0.3
-
-wait(1)
-
-change.Saturation = 0.4
-
-wait(1)
-
-change.Saturation = 0.5
-
-wait(1)
-
-change.Saturation = 0.6
-
-wait(1)
-
-change.Saturation = 0.7
-
-wait(1)
-
-change.Saturation = 0.8
-
-wait(1)
-
-change.Saturation = 0.9
-
-repeat
+wait(0.01)
+change.Saturation = -0
+    end
+end)
